70420fa18975075b0254f6858a0fe7fb9a7740a7
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2024-08-28 14:16:54 +0300

    Update Funktsioonid.cs

--- TARpv23_CSharp/Funktsioonid.cs
+++ TARpv23_CSharp/Funktsioonid.cs
@@ -1,8 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+
+//namespace TARpv23_CSharp
+//{
+//    public class Funktsioonid
+//    {
+//        public static void Tere(string nimi)
+//        {
+//            Console.WriteLine("Tere kallis {0}", nimi);
+//        }
+
+//        public static int Liitmine(int arv1, int arv2)
+//        {
+//            return arv1 + arv2;
+//        }
+//        // Loo Arvuta() funktsiooni, mis sõldub 3 parameetrist: teha, arv1, arv2. Kastuta if konstruktsioon. Tulemus kuva ekraanile.
+//        public static int Korrutamine(int arv1, int arv2)
+//        {
+//            return arv1 * arv2;
+//        }
+//        public static int Lahutamine(int arv1, int arv2)
+//        {
+//            return arv1 - arv2;
+//        }
+//        public static int Jagamine(int arv1, int arv2)
+//        {
+//            return arv1 / arv2;
+//        }
+//    }
+//}
+
 
 namespace TARpv23_CSharp
 {
@@ -12,23 +43,59 @@ namespace TARpv23_CSharp
         {
             Console.WriteLine("Tere kallis {0}", nimi);
         }
-
         public static int Liitmine(int arv1, int arv2)
         {
             return arv1 + arv2;
         }
-        // Loo Arvuta() funktsiooni, mis sõldub 3 parameetrist: teha, arv1, arv2. Kastuta if konstruktsioon. Tulemus kuva ekraanile.
-        public static int Korrutamine(int arv1, int arv2)
+        public static string Pikkuse_analuus(double pikkus)
         {
-            return arv1 * arv2;
+            string analuus;
+            if (pikkus < 1.6)
+            {
+                analuus = "Lühika kasvu inimene";
+            }
+            else if (pikkus < 1.8)
+            {
+                analuus = "Keskmine kasvu inimene";
+            }
+            else
+            {
+                analuus = "Pikka kasvu inimene";
+            }
+            return analuus;
         }
-        public static int Lahutamine(int arv1, int arv2)
+        //päeva number->päeva nimetus
+        public static string Paevad(int nr)
         {
-            return arv1 - arv2;
+            string paev;
+            switch (nr)
+            {
+                case 1: paev = "Esmaspaev"; break;
+                case 2: paev = "Teisipaev"; break;
+                case 3: paev = "Kolmapaev"; break;
+                case 4: paev = "Neljapaev"; break;
+                case 5: paev = "Reede"; break;
+                case 6: paev = "Laupaev"; break;
+                case 7: paev = "Pühapaev"; break;
+                default:
+                    paev = "Ei saa määrata";
+                    break;
+            }
+            return paev;
         }
-        public static int Jagamine(int arv1, int arv2)
+        //ÜLESAND 1
+        public static int[] Arvude_massiv(int N, int M)
         {
-            return arv1 / arv2;
+            int arv;
+            int[] arvud= new int [M-N];
+            for (int i = 0; i < M-N; i++ )
+            {
+                Console.WriteLine ("{0} :", i + 1);
+                arv = N;
+                arvud[i] = arv;
+                N++;
+            }
+            return arvud;
         }
     }
 }
