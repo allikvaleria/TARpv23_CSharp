d4f5f158df8aa1054ad46f9889a32b2f23b625bc
allikvaleria <154507539+allikvaleria@users.noreply.github.com>
2024-09-02 15:59:22 +0300

    Update Auto1.cs

--- TARpv23_CSharp/Auto1.cs
+++ TARpv23_CSharp/Auto1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace TARpv23_CSharp
 {
     internal class Auto1
     {
+        public string Regnumber { get; set; }
+        public Color Varv { get; set; }
+        public Inimene Omanik { get; set; }
+        public Auto1() { }
+        public Auto1(string regnumber, Color varv, Inimene omanik)
+        {
+            Regnumber = regnumber;
+            Varv = varv;
+            Omanik = omanik;
+        }
+        public void KelleOmaAuto()
+        {
+            Console.WriteLine($"{Varv.Name} auto regnumbriga {Regnumber} on {Omanik.Nimi} oma");
+        }
     }
 }
